46a52852378d85db42dab39a662f4ade7f3de913
Travis Hoover <thoov7@gmail.com>
2014-10-23 23:00:08 -0700

    Cleaning up sloppy code

--- app/initializers/redirect.js
+++ app/initializers/redirect.js
@@ -4,46 +4,49 @@ import routeReopen from 'ember-redirect/utils/route-reopen';
 export default {
   name: 'redirect',
 
-  initialize: function(container, app) {
-    app.deferReadiness();
-    var router = container.lookup('router:main');
-    var routerCallbacks = router.router.callbacks;
-    var mockRouter = {
-      resourceNameChain: [],
-      route: function(routeName, options) {
-        this.resourceNameChain.push(routeName);
-        var route = this.container.lookup('route:' + this.resourceNameChain.join('.'));
+    initialize: function(container, app) {
+        app.deferReadiness();
+        var router = container.lookup('router:main'),
+            routerCallbacks = router.router.callbacks,
+            mockRouter = {
+                resourceNameChain: [],
+                route: function(routeName, options) {
+                    var routeObject;
 
-        routeReopen(route, options);
+                    this.resourceNameChain.push(routeName);
+                    routeObject = this.container.lookup('route:' + this.resourceNameChain.join('.'));
+                    routeReopen(routeObject, options);
+                    this.resourceNameChain.pop();
+                },
+                resource: function(resourceName, options, subRoutes) {
+                    var defaultResourceObject,
+                        indexResourceObject;
 
-        this.resourceNameChain.pop();
-      },
-      resource: function(resourceName, options, subRoutes) {
+                    this.resourceNameChain = [resourceName];
 
-        this.resourceNameChain = [resourceName];
+                    if(Ember.typeOf(options) === 'function') {
+                        subRoutes = options;
+                    }
+                    else if(options.redirect) {
+                        defaultResourceObject = this.container.lookup('route:' + resourceName);
+                        indexResourceObject = this.container.lookup('route:' + resourceName + '.index');
 
-        if(Ember.typeOf(options) === 'function') {
-          subRoutes = options;
-        }
-        else if(options.redirect) {
-          var resource = this.container.lookup('route:' + resourceName);
-          routeReopen(resource, options);
+                        routeReopen(defaultResourceObject, options);
+                        routeReopen(indexResourceObject, options);
+                    }
 
-          resource = this.container.lookup('route:' + resourceName + '.index');
-          routeReopen(resource, options);
-        }
+                    subRoutes.call(this);
+                    this.resourceNameChain = [];
+                },
+                container: container
+            };
 
-        subRoutes.call(this);
-        this.resourceNameChain = [];
-      },
-      container: container
-    };
+        routerCallbacks.forEach(function(callback, index) {
+            callback.call(mockRouter, this);
 
-    routerCallbacks.forEach(function(callback, index) {
-      callback.call(mockRouter, this);
-      if((index + 1) === routerCallbacks.length) {
-        app.advanceReadiness();
-      }
-    }, this);
-  }
+            if((index + 1) === routerCallbacks.length) {
+                app.advanceReadiness();
+            }
+        }, this);
+    }
 };
