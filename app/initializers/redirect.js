1a1a9e369e3ecec160cf510ebe2cfdbdb7019720
Travis Hoover <thoov7@gmail.com>
2014-10-28 16:31:14 -0700

    Adding safe redirects

--- app/initializers/redirect.js
+++ app/initializers/redirect.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import createMockRouter from 'ember-redirect/mocks/router';
+import reopenRoute from 'ember-redirect/utils/route-reopen';
 
 export default {
     name: 'redirect',
@@ -7,10 +8,20 @@ export default {
     initialize: function(container, application) {
         var router = container.lookup('router:main'),
             routerCallbacks = router.router.callbacks,
-            mockRouter = createMockRouter(container, application);
+            mockRouter = createMockRouter(container, application),
+            routeNames = (router && router.redirects) ? Ember.keys(router.redirects) : [];
 
-        routerCallbacks.forEach(function(callback, index) {
-            callback.call(mockRouter, this);
-        }, this);
+        // The user has specified the redirects on the router object instead of the map function
+        // so we should use those instead of invoking the map function
+        if(router.redirects && routeNames.length > 0) {
+            Ember.EnumerableUtils.forEach(routeNames, function(routeName) {
+                reopenRoute(routeName, { redirect: router.redirects[routeName] }, container, application);
+            });
+        }
+        else {
+            routerCallbacks.forEach(function(callback, index) {
+                callback.call(mockRouter, this);
+            }, this);
+        }
     }
 };
