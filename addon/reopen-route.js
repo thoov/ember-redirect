5f92b7c8078d91bc9e63e337d8317e31fe7f5d00
Travis Hoover <thoov7@gmail.com>
2014-10-29 13:44:23 -0700

    Cleaning up reopen-route

--- addon/reopen-route.js
+++ addon/reopen-route.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import arraySwap from './utils/array-swap';
 
 export default function(routeName, options, container, application) {
@@ -19,13 +20,11 @@ export default function(routeName, options, container, application) {
 
     routeObject.reopen({
         beforeModel: function(transition) {
-            var newDynamicObject = {},
+            var newDynObject = {},
                 thisRouteName = this.routeName,
                 routeNames = this.router.router.recognizer.names,
-                segmentsOfNextRoute = routeNames[options.redirect].segments,
-                segmentsOfThisRoute = routeNames[thisRouteName].segments,
-                dynamicSegmentsOfNextRoute = segmentsOfNextRoute.filterBy('name').mapBy('name'),
-                dynamicSegmentsOfThisRoute = segmentsOfThisRoute.filterBy('name').mapBy('name');
+                dynSegsOfNextRoute = routeNames[options.redirect].segments.filterBy('name').mapBy('name'),
+                dynSegsOfThisRoute = routeNames[thisRouteName].segments.filterBy('name').mapBy('name');
 
             // Make sure we only try to make a redirect at the most nested
             // route and not a parent resource.
@@ -35,19 +34,20 @@ export default function(routeName, options, container, application) {
 
             // Make sure that the lengths are the same else we are trying to transition to a route that needs more
             // segments then we can supply.
-            if(dynamicSegmentsOfNextRoute.length <= dynamicSegmentsOfThisRoute.length) {
-                dynamicSegmentsOfNextRoute.forEach(function(item, index) {
+            if(dynSegsOfNextRoute.length <= dynSegsOfThisRoute.length) {
+
+                Ember.EnumerableUtils.forEach(dynSegsOfNextRoute, function(item, index) {
                     // This means that we have the same dynamic segment on both this and the next route so we will pair them together.
-                    if(dynamicSegmentsOfThisRoute.contains(dynamicSegmentsOfNextRoute[index])) {
-                        newDynamicObject[dynamicSegmentsOfNextRoute[index]] = transition.params[thisRouteName][dynamicSegmentsOfNextRoute[index]];
-                        dynamicSegmentsOfThisRoute = arraySwap(dynamicSegmentsOfThisRoute, index, dynamicSegmentsOfThisRoute.indexOf(dynamicSegmentsOfNextRoute[index]));
+                    if(dynSegsOfThisRoute.contains(dynSegsOfNextRoute[index])) {
+                        newDynObject[dynSegsOfNextRoute[index]] = transition.params[thisRouteName][dynSegsOfNextRoute[index]];
+                        dynSegsOfThisRoute = arraySwap(dynSegsOfThisRoute, index, dynSegsOfThisRoute.indexOf(dynSegsOfNextRoute[index]));
                     }
                     else {
-                        newDynamicObject[dynamicSegmentsOfNextRoute[index]] = transition.params[thisRouteName][dynamicSegmentsOfThisRoute[index]];
+                        newDynObject[dynSegsOfNextRoute[index]] = transition.params[thisRouteName][dynSegsOfThisRoute[index]];
                     }
                 });
 
-                this.replaceWith(transition.router.recognizer.generate(options.redirect, newDynamicObject));
+                this.replaceWith(transition.router.recognizer.generate(options.redirect, newDynObject));
             }
 
             this._super.apply(this, arguments);
